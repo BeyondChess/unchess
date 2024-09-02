8f2ca77f5cfcbb3ecd8804d3c8ac3b4b94a6765c
substantialcattle5 <nilaynathsharan16@gmail.com>
2024-09-02 17:02:05 +0530

    feat: auth sessions

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,10 @@
+import { PrismaClient } from '@prisma/client';
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+const prisma = new PrismaClient();
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [GitHub],
 });
